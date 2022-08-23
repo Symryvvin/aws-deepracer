1e202096332b032d3b84387a59bf1cd0f7e72f02
ushakov_ri <ushakov_ri@magnit.ru>
2022-08-23 18:42:30 +0300

    add some reward functions

--- reward_function.py
+++ reward_function.py
@@ -1,3 +1,6 @@
+import math
+
+
 def reward_function(params):
     # x and y  - The position of the vehicle on the track
     # heading - Orientation of the vehicle on the track
@@ -25,66 +28,116 @@ def reward_function(params):
     speed = params['speed']
     steering = params['steering_angle']
 
-    # try to build model on calculating coefficient to change result reward
-    # if car make serious mistakes it get minimum coefficient and result reward drops
-    # max coefficient of each function can be optimized through learning
-    MIN_COEFF = 0.05
-    NO_CHANGE_COEF = 1.0
+    MIN_REWARD = 1e-3
     MAX_REWARD = 1.0
-    ABS_STEERING_THRESHOLD = 15
 
     MAX_SPEED = 4.0
     MIN_SPEED = 1.0
 
-    # if car on track
-    def stay_on_track_coef():
+    def stay_car_on_track():
         if all_wheels_on_track and (0.5 * track_width - distance_from_center) >= 0.05:
-            return 1.3
-        return MIN_COEFF
+            return MAX_REWARD * 1.3
+        return MIN_REWARD
 
-    # if near to center line
-    def follow_center_line_coef():
+    def follow_near_center_track_line():
         closer_to_center = 0.1 * track_width
         allowed_distance = 0.25 * track_width
         close_to_off_track = 0.5 * track_width
 
         if distance_from_center <= closer_to_center:
-            return 1.15
+            return MAX_REWARD * 1.1
         elif distance_from_center <= allowed_distance:
-            return 0.5
+            return MAX_REWARD * 0.5
         elif distance_from_center <= close_to_off_track:
-            return 0.2
+            return MAX_REWARD * 0.2
         else:
-            return MIN_COEFF
+            return MIN_REWARD
 
-    # if car not zig-zag
-    def abs_steering_coef():
+    def abs_steering():
+        ABS_STEERING_THRESHOLD = 15
+
         if abs(steering) > ABS_STEERING_THRESHOLD:
-            return 0.8
-        return NO_CHANGE_COEF
+            return MAX_REWARD * 0.8
+        return MAX_REWARD
 
-    # if car move with near maximum speed on straight line, while abs(steering) is minimum
-    def moving_on_straight_line_faster_coef():
+    def moving_on_straight_line_faster():
         if abs(steering) < 0.1:
             if round(speed) == MAX_SPEED:
-                return 1.4
+                return MAX_REWARD * 1.4
             elif round(speed) == MIN_SPEED:
-                return 0.9
+                return MAX_REWARD * 0.9
         elif abs(steering) < 0.2:
             if round(speed) == MAX_SPEED - 1:
-                return 1.2
+                return MAX_REWARD * 1.2
             elif round(speed) == MIN_SPEED:
-                return 0.9
-        return NO_CHANGE_COEF
+                return MAX_REWARD * 0.9
+        return MAX_REWARD
 
-    # if car move to right turn based on waypoint and coordinates
-    # if car move with calm speed on turns
-    # if car end track with minimum steps
+    def track_heading(prev_wp, next_wp):
+        track_heading_in_rads = math.atan2(next_wp[1] - prev_wp[1], next_wp[0] - prev_wp[0])
+        return math.degrees(track_heading_in_rads)
 
-    reward = MAX_REWARD
-    reward *= stay_on_track_coef()
-    reward *= follow_center_line_coef()
-    reward *= abs_steering_coef()
-    reward *= moving_on_straight_line_faster_coef()
+    def wp_by_index(index):
+        if index > (len(waypoints) - 1):
+            return waypoints[index - (len(waypoints))]
+        elif index < 0:
+            return waypoints[len(waypoints) + index]
+        else:
+            return waypoints[index]
+
+    def track_turn_angle(next_wp_index):
+        first_angle = track_heading(wp_by_index(next_wp_index), wp_by_index(next_wp_index + 1))
+        second_angle = track_heading(wp_by_index(next_wp_index), wp_by_index(next_wp_index - 1))
+
+        return first_angle - second_angle
+
+    def left_of_center_when_turn():
+        next_wp_index = closest_waypoints[1]
+        turn_angle = track_turn_angle(next_wp_index)
+
+        if turn_angle > 0:
+            if is_left_of_center:
+                return MAX_REWARD * 1.2
+            else:
+                return MAX_REWARD * 0.7
+        elif turn_angle < 0:
+            if is_left_of_center:
+                return MAX_REWARD * 0.7
+            else:
+                return MAX_REWARD * 1.2
+
+        return MAX_REWARD
+
+    def slow_speed_when_turn():
+        next_wp = closest_waypoints[1]
+        abs_turn_angle = abs(track_turn_angle(next_wp))
+
+        if abs_turn_angle > 20 and MAX_SPEED - 1 > speed > MIN_SPEED + 1:
+            return MAX_REWARD * 1.2
+        elif abs_turn_angle > 40 and MAX_SPEED - 1 > speed > MIN_SPEED + 0.5:
+            return MAX_REWARD * 1.2
+        return MAX_REWARD
+
+    def car_move_in_right_direction():
+        next_wp = closest_waypoints[1]
+        prev_wp = closest_waypoints[0]
+
+        direction_diff = abs(track_heading(prev_wp, next_wp) - heading)
+        if direction_diff > 180:
+            direction_diff = 360 - direction_diff
+
+        DIRECTION_THRESHOLD = 10.0
+        if direction_diff > DIRECTION_THRESHOLD:
+            return MAX_REWARD * 0.5
+
+        return MAX_REWARD
+
+    reward = stay_car_on_track()
+    reward += follow_near_center_track_line()
+    reward += abs_steering()
+    reward += moving_on_straight_line_faster()
+    reward += left_of_center_when_turn()
+    reward += slow_speed_when_turn()
+    reward += car_move_in_right_direction()
 
     return float(reward)
