0d01ed84588e9c8cd5a2a6e598bfc0cd3ebaffb6
ushakov_ri <ushakov_ri@magnit.ru>
2022-08-23 13:36:47 +0300

    add basic example functions

--- reward_function.py
+++ reward_function.py
@@ -26,9 +26,40 @@ def reward_function(params):
     speed = params['speed']
     steering = params['steering_angle']
 
-    NO_REWARD = 0
+    # try to build model on calculating coefficient to change result reward
+    # if car make serious mistakes it get minimum coefficient and result reward drops
+    # max coefficient of each function can be optimized through learning
+    MIN_COEFF = 0.05
     MAX_REWARD = 1.0
+    ABS_STEERING_THRESHOLD = 15
 
-    reward = NO_REWARD
+    def stay_on_track_coef():
+        if all_wheels_on_track and (0.5 * track_width - distance_from_center) >= 0.05:
+            return 1.3
+        return MIN_COEFF
+
+    def follow_center_line_coef():
+        closer_to_center = 0.1 * track_width
+        allowed_distance = 0.25 * track_width
+        close_to_off_track = 0.5 * track_width
+
+        if distance_from_center <= closer_to_center:
+            return 1.15
+        elif distance_from_center <= allowed_distance:
+            return 0.5
+        elif distance_from_center <= close_to_off_track:
+            return 0.2
+        else:
+            return MIN_COEFF
+
+    def abs_steering_coef():
+        if abs(steering) > ABS_STEERING_THRESHOLD:
+            return 0.8
+        return 1.0
+
+    reward = MAX_REWARD
+    reward *= stay_on_track_coef()
+    reward *= follow_center_line_coef()
+    reward *= abs_steering_coef()
 
     return float(reward)
